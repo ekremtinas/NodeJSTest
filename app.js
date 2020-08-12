8a4aaa569a8f8b9522d0bff85808640ce831591a
Ekrem Tinas <ekremtinas@gmail.com>
2020-08-12 17:57:30 +0300

    Express Modülü Kullanılarak Css için Public klasörü public olarak herkese açıldı. -- Version 1.0.2

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var http=require('http');
 var fs=require('fs');
 
 var express=require('express');
- 
+var path = require('path');
 var app=express();
+
+app.use('/public',express.static(path.join(__dirname,'public')));
 
 app.get('/',function(req,res){
     fs.readFile('index.html',function(err,data){
