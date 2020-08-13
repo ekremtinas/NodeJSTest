ad3ad0359fc133d860cc23049d7fe7d2fd7300b2
Ekrem Tinas <ekremtinas@gmail.com>
2020-08-13 16:19:11 +0300

    NodeJs de route için ve controller için modül tasarlandı. Bu yapılar ayrıldı.Bir başka js dosyası require edildi.O dosyada module.export edildi. --- Version 1.0.3

--- app.js
+++ app.js
@@ -6,25 +6,20 @@ var express=require('express');
 var path = require('path');
 var app=express();
 
+var controllerElektronik=require('./ElektronikController');
+
+
 app.use('/public',express.static(path.join(__dirname,'public')));
 
-app.get('/',function(req,res){
-    fs.readFile('index.html',function(err,data){
-        res.write(data);
-        
-        res.end('mesaj bitti');
-    });
-});
 
-app.get('/login',function(req,res){
-    fs.readFile('login.html',function(err,data){
-        res.write(data);
-        
-        res.end('mesaj bitti');
-    });
-});
 
-app.listen(8000);
+
+
+app.get('/',controllerElektronik.home);
+
+app.get('/login',controllerElektronik.bilgisayar);
+
+app.listen(9000);
 
 
 
